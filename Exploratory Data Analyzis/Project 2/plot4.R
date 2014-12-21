34362540cf6c1d750038d362e7f3891bfb511cec
Maxjacu <maxjacu@gmail.com>
2014-12-20 21:09:38 -0500

    Plot 5-6

--- Exploratory Data Analyzis/Project 2/plot4.R
+++ Exploratory Data Analyzis/Project 2/plot4.R
@@ -1,16 +1,14 @@
-require(ggplot2)
-require(data.table)
 #Read data
 NEI  <- readRDS("summarySCC_PM25.rds")
 SCC  <- readRDS("Source_Classification_Code.rds")
 
 #Plot 4 : Across the United States, how have emissions 
 #         from coal combustion-related sources changed from 1999-2008?
-SCC.coal <- as.vector(SCC$SCC[grepl("Coal", SCC$EI.Sector)])
-
+SCC.coal <- as.vector(SCC$SCC[grepl("Coal", SCC$Short.Name)|grepl("Fuel Comb", SCC$Short.Name)])
 NEI.coal <- NEI[NEI$SCC %in% SCC.coal,]
+
 barplot(t(sapply(split(NEI.coal$Emissions, NEI.coal$year), sum)), ylab="PM2.5 emission",
-        main="USA Emission trend 1999 - 2008 from Coal Sources")
+        main="USA Emission trend from Coal Sources")
 
 #Save to png
 dev.copy(png, file = "plot4.png")  ## Copy my plot to a PNG file
---
+++ Exploratory Data Analyzis/Project 2/plot5.R
@@ -0,0 +1,15 @@
+#Read data
+NEI  <- readRDS("summarySCC_PM25.rds")
+SCC  <- readRDS("Source_Classification_Code.rds")
+
+#Plot 5 : How have emissions from motor vehicle sources changed from 1999-2008 in Baltimore City? 
+NEI.B <- NEI[NEI$fips == "24510",]
+SCC.mv <- SCC$SCC[SCC$Data.Category == "Onroad"]
+NEI.B.mv <- NEI.B[NEI.B$SCC %in% SCC.mv,]
+
+barplot(t(sapply(split(NEI.B.mv$Emissions, NEI.B.mv$year), sum)), ylab="PM2.5 emission",
+        main="USA Emission trend from mobile vehicles")
+
+#Save to png
+dev.copy(png, file = "plot5.png")  ## Copy my plot to a PNG file
+dev.off() 
